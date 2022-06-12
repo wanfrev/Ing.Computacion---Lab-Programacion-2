5431cf8af93bd0bfc6d5232944c05a0b5afccc4a
Wanfredo Vargas <84217875+wanfrev@users.noreply.github.com>
2022-06-12 17:57:00 -0500

    Add files via upload

---
+++ Asignacion 2/Fibonacci-recursividad.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int fibonacci(int i);
+
+int main(){
+	int n;
+	
+	printf("Digite hasta donde llegara la serie: "); scanf("%d",&n);
+	
+	for(int i=0;i<n;i++){
+		printf("%i, ",fibonacci(i));
+	}
+	
+	getch();
+	return 0;
+}
+
+int fibonacci(int n){
+	if(n<2){
+		return n;
+	}else{
+		return fibonacci(n-1)+fibonacci(n-2);
+	}
+}
